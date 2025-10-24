f1a84a44c0293df033b01cbb5a4ab348591f1e01
Joseph <josephkim0703@gmail.com>
2025-10-23 17:21:34 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+
+import Main from './components/Main.jsx';
+import './css/App.css'
+function App() {
+  return (
+    <>
+    <Main/>
+    </>
+  )
+
+}
+export default App;
+
